f1d7dc4f779788f998212d3fce2226b02a1d0dd4
CZKAH <czkuster@gmail.com>
2025-11-01 20:08:11 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.aeroporto;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+       
+
+        SwingUtilities.invokeLater(TelaInicial::new);
+
+    }
+}
